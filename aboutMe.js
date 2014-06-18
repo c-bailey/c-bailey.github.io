dfabdf792913cee1af0bbf25b63bbc58a3f4d0d2
Chelsea <chelsea.bailey@students.olin.edu>
2014-06-17 23:53:18 -0400

    pretty AboutMe

--- aboutMe.js
+++ aboutMe.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
 	var duration = 500;
 
 	$('.aboutMe').hover(function() {
-		$(this).stop(true, false).animate({left: 0}, duration);
+		$(this).stop(true, false).animate({left: -10}, duration);
 	}, function() {
-		$(this).stop(true, false).animate({left: -695}, duration);
+		$(this).stop(true, false).animate({left: -705}, duration);
 	});
 
 });
