849736097409774e3be19128b63e2ce8c2f130f6
Chelsea <chelsea.bailey@students.olin.edu>
2014-06-21 00:03:32 -0400

    menu addition

--- js/populate.js
+++ js/populate.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
 	$('.topPanel').load('index.html .topPanel');
+	$('.aboutMe').load('index.html .aboutMe');
+	$('.menu').load('menu.html .menu')
 	$('#footer').load('index.html #footer');
 });
