f8908735a613d050f4b78fb41394e0aaf28341fe
Chelsea <chelsea.bailey@students.olin.edu>
2014-06-27 17:16:29 -0400

    reverting to single page

--- js/populate.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	$('.topPanel').load('index.html .topPanel');
-	$('.aboutMe').load('index.html .aboutMe');
-	$('.menu').load('menu.html .menu')
-	$('#footer').load('index.html #footer');
-});
